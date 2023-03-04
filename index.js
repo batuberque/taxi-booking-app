b586b541f455ff311413eb9ae257ebb3fe50fa2d
batuberque <batuberque@gmail.com>
2023-03-05 01:15:09 +0300

    feature: create base service for crud

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const app = require('express')
+const express = require('express')
 const bodyParser = require('body-parser')
+require('./mongo-connection')
+
 
 const app = express()
 app.use(bodyParser.json())
